0593768cce8caf4aef673065f53bba11e0b25c60
knknk98 <kayooshino@gmail.com>
2021-06-23 20:33:56 +0900

    プルリク関連の操作

--- pullrequest.py
+++ pullrequest.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 from github import Github
 from local_settings import *
+import random
+from datetime import datetime, timedelta
 # PullRequestの操作
 
 token = MY_OAUTH_TOKEN
 g = Github(token)
 repo = g.get_repo(REPO_NAME)
 
+# レビュアーをランダムで1人決める
+members = list(repo.get_collaborators())
+randomized_member = random.choice(members)
+print(randomized_member.login)
+
+# プルリクにランダムでレビュアー/assignees設定
 pulls = repo.get_pulls(state='open', sort='created', base='main')
 for pr in pulls:
-    print(pr.title)
+    pr.create_review_request([randomized_member.login])
+    pr.add_to_assignees(randomized_member)
+
+# m分以上更新されていないプルリク取得
+# Get time now
+now = datetime.now()
+m = 10
+for p in pulls:
+    if now - p.updated_at > timedelta(minutes=m):
+        print(p.title + 'がしばらく更新されていません')
