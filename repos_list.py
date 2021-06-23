3b40912f8a3bded58e26aa6e541579af91e21cc6
knknk98 <kayooshino@gmail.com>
2021-06-23 19:09:10 +0900

    リポジトリ一覧取得

---
+++ repos_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from github import Github
+from local_settings import *
+# 自分のリポジトリ一覧を取得
+
+token = MY_OAUTH_TOKEN
+
+g = Github(token)
+
+print("🦀自分のリポジトリ一覧を取得🦀")
+print("[名前,最終更新日時,デフォルトブランチ,openしてるissueの数]")
+for repo in g.get_user().get_repos(type='owner'):
+    print(repo.name, repo.updated_at, repo.default_branch, repo.open_issues_count)
+
+print("🦀Contributors一覧取得🦀")
+for repo in g.get_user().get_repos(type='owner'):
+    print([repo.name])
+    for contributer in repo.get_contributors():
+        print(contributer.login)
